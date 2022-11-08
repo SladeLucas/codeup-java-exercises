0301d7da69afc3a9759dd85c529b68bbbf85cc41
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-08 10:19:34 -0700

    finished the syntax exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,8 +10,27 @@ public class HelloWorld {
         double myNumber = 3.14;
         //long is wider version of int, where double or float allow for decimal values
         System.out.println(myNumber);
-        int x = 5;
-        System.out.println(++x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+        //It is similar to js where the increment is pre or post
+        //int class = 4; does not work class is already a thing in java
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        //int three = (int) "three";
+        //Strings and objects can't be converted to int
+        int x = 4;
+        x += 5;
+        x = 3;
+        int y = 4;
+        y *= x;
+        x = 10;
+        y = 2;
+        x /= y;
+        y -= x;
         System.out.println(x);
+        System.out.println(y);
+        int maxNum = Integer.MAX_VALUE;
+        System.out.println(maxNum);
     }
 }
