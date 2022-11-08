43018dab1046fd8cf0dcbee72f0069ea0651ca14
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-08 09:46:52 -0700

    about half way through the exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,16 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello,");
         System.out.print(" World!");
+
+        int myFavoriteNumber = 21;
+        System.out.println(myFavoriteNumber);
+        String myString = "string value";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        //long is wider version of int, where double or float allow for decimal values
+        System.out.println(myNumber);
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
     }
 }
