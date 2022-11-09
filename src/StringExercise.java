f9b25b2678656ce798146d245aa6c5c4266e8347
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-09 15:53:14 -0700

    finished the exercise associated with file

---
+++ src/StringExercise.java
@@ -0,0 +1,12 @@
+public class StringExercise {
+    public static void main(String[] arg){
+        String oldsong = "We don't need to education %nWe don't need no thought control%n";
+        System.out.printf(oldsong);
+        String slashes = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(slashes);
+        String window = "In windows, the main drive is usually C:\\";
+        System.out.println(window);
+        String backslash = "I can do backslashes\\, double backslashes \\\\,%n and the amazing triple backslash \\\\\\!";
+        System.out.printf(backslash);
+    }
+}
