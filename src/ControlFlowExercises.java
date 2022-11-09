64e2b18b7de3964f6d8e91d28c70446432c6a54c
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-09 10:26:53 -0700

    finsihed question one in the exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,33 @@
+public class ControlFlowExercises {
+    public static void main(String[] arg) {
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.print(i + " ");
+//            i++;
+//        }
+//        int num = 100;
+//        do {
+//           System.out.print(num + " ");
+//           num-=5;
+//        } while (num >= -10);
+
+//        int x = 2;
+//        System.out.println(x);
+//        do {
+//            x = (int) Math.pow(x, 2);
+//            System.out.println(x);
+//        } while (x < 65536);
+//Refactor to For loop
+//        for (int i = 0; i <= 100; i += 2) {
+//            System.out.println(i);
+//        for (int i = 2; i < 65535; ) {
+//            i = (int) Math.pow(i, 2);
+//            System.out.println(i);
+//            }
+//        }
+
+
+
+
+    }
+}
