e2da655290cf055256051d7fdc7ea703431dac41
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-09 10:35:56 -0700

    added some FIZZBUZZIN

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -25,8 +25,19 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            }
 //        }
+// FIZZBUZZ FUN
+        for (int i = 1; i <= 100; i++) {
 
-
+            if (i % 15 == 0){
+                System.out.println("Fizzbuzz");
+            } else if (i % 5 == 0){
+                System.out.println("Buzz");
+            } else if (i % 3 == 0){
+                System.out.println("Fizz");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
 
 
     }
