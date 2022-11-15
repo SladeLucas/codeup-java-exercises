48e356f166c4bd4036f3b5b240f73f8625859736
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-15 13:16:18 -0700

    created the proper methods

---
+++ src/shapes/Circle.java
@@ -0,0 +1,16 @@
+package shapes;
+
+public class Circle {
+    private double radius;
+
+    public Circle(double radius){
+
+    }
+    public double getArea(){
+
+    }
+    public double getCircumference(){
+
+    }
+
+}
