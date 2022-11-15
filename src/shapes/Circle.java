06a4649d70fb3d513a9b0e4b60abf2bd91848f49
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-15 13:22:55 -0700

    added uses for the methods

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,16 +1,21 @@
 package shapes;
 
+import util.Input;
+
 public class Circle {
     private double radius;
 
     public Circle(double radius){
-
+        radius = Input.getDouble();
+        System.out.println("this.radius = " + this.radius);
     }
     public double getArea(){
-
+        double area = (Math.PI * (this.radius * this.radius));
+        return area;
     }
     public double getCircumference(){
-
+        double circumference = (2 * Math.PI * this.radius);
+        return circumference;
     }
 
 }
