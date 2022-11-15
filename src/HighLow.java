8d6337072b7a008b0e9cd7bd43781e4ed5041296
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-15 09:19:55 -0700

    I made a game yay

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.lang.Math;
 
 public class HighLow {
 
@@ -9,13 +10,37 @@ public class HighLow {
         String answer = scanner.next();
         boolean play = answer.equalsIgnoreCase("y");
         do {
-            Random theNumber = new Random(100);
-
+            int min = 1;
+            int max = 100;
+            int theNumber = (int)(Math.random()*(max-min+1)+min);
+            //System.out.println("the Number = " + theNumber);
+            System.out.println("Guess a number between 1 and 100...");
+            int guess = scanner.nextInt();
+            System.out.println("guess = " + guess);
+            while (guess != theNumber) {
+                if (guess < theNumber) {
+                    System.out.println("HIGHER");
+                    System.out.println("Try again");
+                    guess = scanner.nextInt();
+                    System.out.println("guess = " + guess);
+                } else if (guess > theNumber) {
+                    System.out.println("LOWER");
+                    System.out.println("Try again");
+                    guess = scanner.nextInt();
+                    System.out.println("guess = " + guess);
+                }
+            }
+            if (guess == theNumber){
+                System.out.println("GOOD GUESS");
+            }
+            System.out.println("shall we play again? Y/N");
+            answer = scanner.next();
+            play = answer.equalsIgnoreCase("y");
         } while (play);
-        return numGuess;
+        return x;
     }
 
     public static void main(String[] arg){
-
+        numGuess(1);
     }
 }
