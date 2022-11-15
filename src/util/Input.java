97647cf01df476f8efd25b15ed22f0b7d388c704
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-15 11:09:57 -0700

    a bunch of mathods have been made

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,7 +1,31 @@
 package util;
-
+import java.util.Scanner;
 public class Input {
-    private void scanner(){
+    private static Scanner pscanner = new Scanner(System.in);
+
+    public static String getString(){
+        String retstring = pscanner.nextLine();
+        return retstring;
+    }
+    //The yesNo method should return true if the user enters y, yes, or variants thereof, and false otherwise.
+    public static boolean yesNo(){
+        String input = pscanner.nextLine();
+        if (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public static int getInt(int min, int max){
+
+    }
+    public static int getInt(){
+
+    }
+    public static double getDouble(double min, double max){
+
+    }
+    public static double getDouble(){
 
     }
 }
