d294add4f5ffc87318cbb01c729756053d9eb45c
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-15 11:24:01 -0700

    the getInt is made

--- src/util/Input.java
+++ src/util/Input.java
@@ -17,7 +17,17 @@ public class Input {
         }
     }
     public static int getInt(int min, int max){
-
+        System.out.printf("Enter a number between %d and %d: %n", min, max);
+        int input = Integer.parseInt(pscanner.nextLine());
+            if (input < min) {
+                System.out.println("Try a higher number: ");
+                return getInt(min, max);
+            } else if (input > max){
+                System.out.println("Try a lower number: ");
+                return getInt(min, max);
+            }
+        System.out.printf("That number %d is between %d and %d", input, min, max);
+        return input;
     }
     public static int getInt(){
 
