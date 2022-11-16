3282c33deefd8ceaa47575181ad68c247b973621
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-16 09:11:03 -0700

    created some objects and arrays

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,6 @@
+public class ServerNameGenerator {
+
+    static String[] nouns = {"house", "Chicago", "Island", "Water", "Box", "Car", "Lamp", "Foot", "Licorice", "Laptop"};
+    static String[] adjectives = {"floppy", "limp", "strange", "smelly", "confused", "content", "happy", "sad", "suspicious", "sleepy"};
+
+}
