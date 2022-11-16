d87dcdebf8b5a9009dcceeeb04075eca447090c3
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-16 08:08:18 -0700

    finished with the OOP

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,4 +1,18 @@
 package shapes;
+import util.Input;
 
 public class CircleApp {
+    public static void main(String[] arg) {
+        boolean response = true;
+        while(response){
+            System.out.printf("What is the radius of your circle?%n");
+            Circle circle = new Circle(6);
+
+
+            System.out.printf("TWould you like to create another circle?%n");
+            response = Input.yesNo();
+        };
+        System.out.printf("Guess we will see you around.%n");
+    }
 }
+
