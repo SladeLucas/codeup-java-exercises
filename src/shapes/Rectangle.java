e70f044223ba46e219c3626f6b572322d1ade222
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-17 13:08:24 -0700

    squares will be created

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,12 +1,20 @@
 package shapes;
 
-public class Rectangle {
+public class Rectangle{
     protected int length;
     protected int width;
 
-    public Rectangle(int x, int y){
-        x = length;
-        y = width;
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
     }
     public int getArea(){
         int area = this.length * this.width;
---
+++ src/shapes/Square.java
@@ -0,0 +1,23 @@
+package shapes;
+
+public class Square extends Rectangle{
+
+    public Square(int side){
+        super(side, side);
+    }
+    public void setWidth(int width) {
+        this.width = width;
+        this.length = width;
+    }
+    public void setLength(int length) {
+        this.length = length;
+        this.width = length;
+    }
+    public int getPerimeter() {
+
+        return this.length * 4;
+    }
+    public int getArea() {
+        return this.length * this.length;
+    }
+}
