7733383c79bbb2e860ad0580e06526439e08eb0e
Lucas Slade <lucas.sheldon.slade@gmail.com>
2022-11-18 14:55:51 -0700

    added the quadrilateral

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,27 +1,49 @@
 package shapes;
 
 public class Rectangle{
-    protected int length;
-    protected int width;
-
-    public Rectangle(int length, int width){
-        this.length = length;
-        this.width = width;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-    public int getArea(){
-        int area = this.length * this.width;
-        return area;
-    }
-    public int getPerimeter(){
-        int perimeter = 2 * this.length + 2 * this.width;
-        return perimeter;
-    }
+//    protected int length;
+////    protected int width;
+////
+////    public Rectangle(int length, int width){
+////        this.length = length;
+////        this.width = width;
+////    }
+////
+////    public void setWidth(int width) {
+////        this.width = width;
+////    }
+////
+////    public void setLength(int length) {
+////        this.length = length;
+////    }
+////    public int getArea(){
+////        int area = this.length * this.width;
+////        return area;
+////    }
+////    public int getPerimeter(){
+////        int perimeter = 2 * this.length + 2 * this.width;
+////        return perimeter;
+////    }protected int length;
+//    protected int width;
+//
+//    public Rectangle(int length, int width){
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public void setWidth(int width) {
+//        this.width = width;
+//    }
+//
+//    public void setLength(int length) {
+//        this.length = length;
+//    }
+//    public int getArea(){
+//        int area = this.length * this.width;
+//        return area;
+//    }
+//    public int getPerimeter(){
+//        int perimeter = 2 * this.length + 2 * this.width;
+//        return perimeter;
+//    }
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -2,24 +2,24 @@ package shapes;
 
 public class Square extends Rectangle{
 
-    public Square(int side){
-        super(side, side);
-    }
-    public void setWidth(int width) {
-        this.width = width;
-        this.length = width;
-    }
-    public void setLength(int length) {
-        this.length = length;
-        this.width = length;
-    }
-    @Override
-    public int getPerimeter() {
-
-        return this.length * 4;
-    }
-    @Override
-    public int getArea() {
-        return this.length * this.length;
-    }
+//    public Square(int side){
+//        super(side, side);
+//    }
+//    public void setWidth(int width) {
+//        this.width = width;
+//        this.length = width;
+//    }
+//    public void setLength(int length) {
+//        this.length = length;
+//        this.width = length;
+//    }
+//    @Override
+//    public int getPerimeter() {
+//
+//        return this.length * 4;
+//    }
+//    @Override
+//    public int getArea() {
+//        return this.length * this.length;
+//    }
 }
